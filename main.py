6f28ad09bc853153bf836674690f659c9a5d7b43
Pezcodeur <pez.courrier@gmail.com>
2026-06-21 20:50:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes
 
